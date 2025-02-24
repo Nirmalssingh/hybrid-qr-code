128a82979f7a10568a746055e9e1dae460069e99
Nirmalssingh <114557949+Nirmalssingh@users.noreply.github.com>
2025-02-24 21:04:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'hybrid-cache-v1';
+const urlsToCache = [
+  '/',
+  '/hybrid_qr.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
